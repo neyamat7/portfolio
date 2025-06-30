acf541ef9579feb9ddf8f3ee78bab6266ab709e6
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-06-30 17:11:45 +0600

    modify animated circles from HeroSection component

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -20,31 +20,6 @@ const HeroSection = () => {
           }}
         ></motion.div>
 
-        {/* Small Circles */}
-        <motion.div
-          className="absolute top-1/3 right-1/3 w-4 h-4 bg-purple-400 rounded-full"
-          animate={{ scale: [1, 1.5, 1] }}
-          transition={{ duration: 2, repeat: Number.POSITIVE_INFINITY }}
-        ></motion.div>
-        <motion.div
-          className="absolute bottom-1/3 left-1/4 w-6 h-6 bg-blue-400 rounded-full"
-          animate={{ scale: [1, 1.2, 1] }}
-          transition={{
-            duration: 3,
-            repeat: Number.POSITIVE_INFINITY,
-            delay: 0.5,
-          }}
-        ></motion.div>
-        <motion.div
-          className="absolute top-2/3 left-1/3 w-3 h-3 bg-pink-400 rounded-full"
-          animate={{ scale: [1, 1.8, 1] }}
-          transition={{
-            duration: 2.5,
-            repeat: Number.POSITIVE_INFINITY,
-            delay: 1,
-          }}
-        ></motion.div>
-
         {/* Lines */}
         <div className="absolute top-1/2 left-0 w-32 h-0.5 bg-gradient-to-r from-transparent to-purple-400/50 transform rotate-45"></div>
         <div className="absolute bottom-1/4 right-1/4 w-24 h-0.5 bg-gradient-to-r from-blue-400/50 to-transparent transform -rotate-12"></div>
