95ac60f8aa9459f4648b33d763fcf8c3a7339a66
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-06-30 17:03:33 +0600

    fix: correct import path casing for RootLayout component

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
-import RootLayout from "../layouts/RootLayout";
+import RootLayout from "../Layouts/RootLayout";
 import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
