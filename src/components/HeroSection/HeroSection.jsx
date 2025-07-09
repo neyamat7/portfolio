b92922194ca175db24048c7cbdd27976326a40e7
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-07-09 22:25:58 +0600

    update layout in HeroSection component for improved responsiveness

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <section
       id="home"
-      className="min-h-screen bg-gradient-to-br from-gray-900 via-slate-900 to-black relative overflow-hidden"
+      className=" lg:pt-10 min-h-screen bg-gradient-to-br from-gray-900 via-slate-900 to-black relative overflow-hidden"
     >
       {/* Geometric Background Elements */}
       <div className="absolute inset-0 overflow-hidden">
@@ -26,7 +26,7 @@ const HeroSection = () => {
       </div>
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 relative z-10">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 lg:gap-8 items-center min-h-[80vh] mt-12 lg:mt-0">
+        <div className="flex flex-col lg:flex-row lg:justify-center gap-12 lg:gap-28 items-center min-h-[80vh] mt-12 lg:mt-0">
           {/* Left Side - Content */}
           <motion.div
             className="lg:col-span-7 order-2 lg:order-1"
